800b7a33b0d6040ae5bff6356f18b866367efb9c
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2021-09-22 12:57:31 -0400

    This is Version 2 of my hello world program.

---
+++ Hellomyworld.py2.py
@@ -0,0 +1,10 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:25:35) 
+[Clang 12.0.5 (clang-1205.0.22.11)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # This is Version 1 of my hello world program
+print('Hello my name is Adnan')
+Hello my name is Adnan
+>>> # This is Version 2 of my hello world program
+>>> print('Hello my name is Adnan')
+Hello my name is Adnan
+>>> 
